0f561a38773e2ee3852dfe8bc418b26f9800e8bd
qaws7791 <qaws7791@gmail.com>
2024-04-15 23:54:54 +0900

    Feat: zod를 상용한 환경변수 검증

--- apps/server/src/lib/env.ts
+++ apps/server/src/lib/env.ts
@@ -1,12 +1,16 @@
 import dotenv from "dotenv";
+import { z } from "zod";
 dotenv.config();
 
-const env = {
-  POSTGRES_USER: process.env.POSTGRES_USER,
-  POSTGRES_PASSWORD: process.env.POSTGRES_PASSWORD,
-  POSTGRES_HOST: process.env.POSTGRES_HOST,
-  POSTGRES_PORT: process.env.POSTGRES_PORT,
-  POSTGRES_DB: process.env.POSTGRES_DB,
-};
+const envSchema = z.object({
+  POSTGRES_USER: z.string(),
+  POSTGRES_PASSWORD: z.string(),
+  POSTGRES_HOST: z.string(),
+  POSTGRES_PORT: z.string(),
+  POSTGRES_DB: z.string(),
+  AWS_S3_ACCESS_KEY_ID: z.string(),
+  AWS_S3_SECRET_ACCESS_KEY: z.string(),
+});
 
+const env = envSchema.parse(process.env);
 export default env;
