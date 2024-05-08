052ddf8c35ff287eab0f8b619dd4dfe07fdb942f
qaws7791 <qaws7791@gmail.com>
2024-05-08 22:35:33 +0900

    Feat: nextconfig에 cloudfront 주소 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "d2ttts4ku10mha.cloudfront.net",
+        port: "",
+        pathname: "/uploads/**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
