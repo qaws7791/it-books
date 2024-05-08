c1f52964183794e211db8710d4d8f00f08fece8d
qaws7791 <qaws7791@gmail.com>
2024-05-08 23:05:12 +0900

    Fix: category 가져오기 수정

--- src/app/(admin)/admin/categories/_components/CategoryList.tsx
+++ src/app/(admin)/admin/categories/_components/CategoryList.tsx
@@ -6,7 +6,7 @@ export default function CategoryList() {
   const { data } = useCategoriesQuery();
   return (
     <div className="w-80 h-96 border border-neutral-900 rounded-3xl p-8 flex flex-col gap-2">
-      {data.map((category) => (
+      {data.data.map((category) => (
         <div key={category.id}>{category.name}</div>
       ))}
     </div>
