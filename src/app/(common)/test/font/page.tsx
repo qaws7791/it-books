2a9876de6b9015eb7d7fac523a3297fb681bcabc
qaws7791 <qaws7791@gmail.com>
2024-05-09 15:46:13 +0900

    Feat: 폰트 테스트용 팬그램 추가

--- src/app/(common)/test/font/page.tsx
+++ src/app/(common)/test/font/page.tsx
@@ -3,29 +3,44 @@ import Page from "@/src/shared/components/layout/Page";
 export default function FontTestPage() {
   return (
     <Page className="font-sans">
-      <h1 className="text-9xl">Text 9xl</h1>
-      <h1 className="text-8xl">Text 8xl</h1>
-      <h1 className="text-7xl">Text 7xl</h1>
-      <h1 className="text-6xl">Text 6xl</h1>
-      <h1 className="text-5xl">Text 5xl</h1>
-      <h1 className="text-4xl">Text 4xl</h1>
-      <h1 className="text-3xl">Text 3xl</h1>
-      <h1 className="text-2xl">Text 2xl</h1>
-      <h1 className="text-xl">Text xl</h1>
-      <h1 className="text-lg">Text lg</h1>
-      <h1 className="text-base">Text base</h1>
-      <h1 className="text-sm">Text sm</h1>
-      <h1 className="text-xs">Text xs</h1>
+      <h2 className="text-9xl">Text 9xl</h2>
+      <h2 className="text-8xl">Text 8xl</h2>
+      <h2 className="text-7xl">Text 7xl</h2>
+      <h2 className="text-6xl">Text 6xl</h2>
+      <h2 className="text-5xl">Text 5xl</h2>
+      <h2 className="text-4xl">Text 4xl</h2>
+      <h2 className="text-3xl">Text 3xl</h2>
+      <h2 className="text-2xl">Text 2xl</h2>
+      <h2 className="text-xl">Text xl</h2>
+      <h2 className="text-lg">Text lg</h2>
+      <h2 className="text-base">Text base</h2>
+      <h2 className="text-sm">Text sm</h2>
+      <h2 className="text-xs">Text xs</h2>
 
-      <h1 className="text-3xl font-black">Font Black</h1>
-      <h1 className="text-3xl font-extrabold">Font Extra Bold</h1>
-      <h1 className="text-3xl font-bold">Font Bold</h1>
-      <h1 className="text-3xl font-semibold">Font Semi Bold</h1>
-      <h1 className="text-3xl font-medium">Font Medium</h1>
-      <h1 className="text-3xl font-normal">Font Normal</h1>
-      <h1 className="text-3xl font-light">Font Light</h1>
-      <h1 className="text-3xl font-extralight">Font Extra Light</h1>
-      <h1 className="text-3xl font-thin">Font Thin</h1>
+      <h2 className="text-3xl font-extrabold">✅Font Extra Bold(800)</h2>
+      <p className="text-3xl font-extrabold">
+        다람쥐 헌 쳇바퀴에 타고파
+        <br />
+        The quick brown fox jumps over the lazy dog
+      </p>
+      <h2 className="text-3xl font-bold">✅Font Bold(700)</h2>
+      <p className="text-3xl font-bold">
+        다람쥐 헌 쳇바퀴에 타고파
+        <br />
+        The quick brown fox jumps over the lazy dog
+      </p>
+      <h2 className="text-3xl font-medium">✅Font Medium(500)</h2>
+      <p className="text-3xl font-medium">
+        다람쥐 헌 쳇바퀴에 타고파
+        <br />
+        The quick brown fox jumps over the lazy dog
+      </p>
+      <h2 className="text-3xl font-light">✅Font Light(300)</h2>
+      <p className="text-3xl font-light">
+        다람쥐 헌 쳇바퀴에 타고파
+        <br />
+        The quick brown fox jumps over the lazy dog
+      </p>
     </Page>
   );
 }
