e549ecf81f4d7ef49981eb870a5dd74fa890b27f
qaws7791 <qaws7791@gmail.com>
2024-05-12 21:52:35 +0900

    Refactor: 상수 분리

---
+++ src/books/constants/index.ts
@@ -0,0 +1,14 @@
+export const BOOK_BUY_LINKS = [
+  {
+    site: "YES24",
+    url: "https://www.yes24.com/product/search?query=",
+  },
+  {
+    site: "교보문고",
+    url: "https://search.kyobobook.co.kr/search?keyword=",
+  },
+  {
+    site: "알라딘",
+    url: "https://www.aladin.co.kr/search/wsearchresult.aspx?SearchTarget=Book&SearchWord=",
+  },
+];
