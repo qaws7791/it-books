eee73e34e084601d7ba89d447da596855bb3978b
qaws7791 <qaws7791@gmail.com>
2024-05-16 22:52:01 +0900

    Fix: <PageContainer>로 감싸기

--- src/app/(admin)/admin/categories/page.tsx
+++ src/app/(admin)/admin/categories/page.tsx
@@ -1,12 +1,13 @@
 import CategoryForm from "@/src/app/(admin)/admin/categories/_components/category-form";
 import CategoryList from "@/src/app/(admin)/admin/categories/_components/category-list";
+import PageContainer from "@/src/shared/components/layout/page-container";
 
 export default function AdminCategoriesPage() {
   return (
-    <div>
+    <PageContainer>
       <h1 className="text-2xl font-bold">카테고리 관리</h1>
       <CategoryList />
       <CategoryForm />
-    </div>
+    </PageContainer>
   );
 }
