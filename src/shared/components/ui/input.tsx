4d2ef2671c14b391e425267cb4eedf7289b5996c
qaws7791 <qaws7791@gmail.com>
2024-05-16 17:10:05 +0900

    Style: Input 높이 수정

--- src/shared/components/ui/input.tsx
+++ src/shared/components/ui/input.tsx
@@ -10,7 +10,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type}
         className={cn(
-          "flex w-full rounded-lg border border-[#655f3a] bg-transparent p-3 text-sm text-[#655f3a] file:border-0 hover:bg-neutral-100 file:bg-transparent file:text-sm file:font-medium placeholder:text-[#655f3a] focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-black focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+          "flex w-full h-10 rounded-lg border border-[#655f3a] bg-transparent p-3 text-sm text-[#655f3a] file:border-0 hover:bg-neutral-100 file:bg-transparent file:text-sm file:font-medium placeholder:text-[#655f3a] focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-black focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
           className,
         )}
         ref={ref}
