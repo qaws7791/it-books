ab2ca7eb795e2f69b37f309ab96bdc83cda10cb2
qaws7791 <qaws7791@gmail.com>
2024-05-23 23:37:06 +0900

    Style: Button 스타일 수정

--- src/shared/components/ui/button.tsx
+++ src/shared/components/ui/button.tsx
@@ -1,19 +1,21 @@
+import { cn } from "@/src/shared/lib/utils";
+import { Slot } from "@radix-ui/react-slot";
 import { type VariantProps, cva } from "class-variance-authority";
 import * as React from "react";
-import { Slot } from "@radix-ui/react-slot";
-import { cn } from "@/src/shared/lib/utils";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap font-medium text-sm rounded-full disabled:cursor-not-allowed",
+  "inline-flex items-center justify-center whitespace-nowrap font-medium text-sm rounded-full disabled:cursor-not-allowed relative state-layer",
   {
     variants: {
       variant: {
-        default: "bg-primary text-on-primary hover:bg-primary-dark",
+        default:
+          "bg-primary text-on-primary hover:after:bg-on-primary/8  active:after:bg-on-primary/12 focus:after:bg-on-primary/12 disabled:bg-transparent disabled:after:bg-on-surface/12 disabled:text-on-surface/40",
         secondary:
-          "bg-secondary-container text-on-secondary-container hover:bg-secondary-container-dark",
+          "bg-secondary-container text-on-secondary-container hover:after:bg-on-secondary-container/8 focus:after:bg-on-secondary-container/12 active:after:bg-on-secondary-container/12 disabled:bg-transparent disabled:after:bg-on-surface/12 disabled:text-on-surface/40",
         outline:
-          "border border-primary text-primary bg-transparent hover:bg-primary/10",
-        ghost: "bg-transparent text-black hover:bg-primary/10",
+          "bg-transparent text-primary border border-outline hover:after:bg-primary/8 active:after:bg-primary/12  focus:after:bg-primary/12 disabled:after:bg-transparent disabled:border-on-surface/12 disabled:text-on-surface/40",
+        ghost:
+          "bg-transparent text-primary hover:after:bg-primary/8 active:after:bg-primary/12 focus:after:bg-primary/12 disabled:after:bg-transparent disabled:text-on-surface/40",
       },
       size: {
         default: "h-10 px-6 py-2",
