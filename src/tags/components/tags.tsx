a74ae850c877d3e7f34f69c748ad94090c45f488
qaws7791 <qaws7791@gmail.com>
2024-05-23 23:37:46 +0900

    Feat: tags를 Chip 컴포넌트로 변경

--- src/tags/components/tags.tsx
+++ src/tags/components/tags.tsx
@@ -1,6 +1,6 @@
+import Chip from "@/src/shared/components/ui/chip";
 import { Tag } from "@/src/tags/types";
 import Link from "next/link";
-import React from "react";
 
 interface TagsProps {
   tags: Tag[];
@@ -10,14 +10,11 @@ export default function Tags({ tags }: TagsProps) {
   return (
     <div className="flex gap-4 flex-wrap">
       {tags.map((tag) => (
-        <span
-          key={tag.id}
-          className="flex border border-primary rounded-lg text-sm line-clamp-1 whitespace-nowrap"
-        >
+        <Chip key={tag.id} asChild>
           <Link href={`/tags/${tag.name}`} className="px-4 py-1">
             {tag.name}
           </Link>
-        </span>
+        </Chip>
       ))}
     </div>
   );
