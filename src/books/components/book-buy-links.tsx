e7306756d3d3ba64df7b0e7543c03ca5812d5da1
qaws7791 <qaws7791@gmail.com>
2024-05-24 23:41:28 +0900

    Feat: 새 탭에서 열리도록 수정

--- src/books/components/book-buy-links.tsx
+++ src/books/components/book-buy-links.tsx
@@ -10,8 +10,10 @@ export default function BookBuyLinks({ isbn }: BookBuyLinksProperties) {
   return (
     <div className="flex flex-col gap-4 mt-2">
       {BOOK_BUY_LINKS.map((link) => (
-        <Button key={link.site}>
-          <Link href={`${link.url}${isbn}`}>{link.site}에서 구매하기</Link>
+        <Button key={link.site} asChild>
+          <Link href={`${link.url}${isbn}`} target="_blank">
+            {link.site}에서 구매하기
+          </Link>
         </Button>
       ))}
     </div>
