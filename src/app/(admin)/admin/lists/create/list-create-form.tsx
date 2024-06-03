8fe9e962e3bd933692e15468f57d1524016854e3
qaws7791 <qaws7791@gmail.com>
2024-06-04 07:48:31 +0900

    Refactor: DraggableBookList 분리

--- src/app/(admin)/admin/lists/create/list-create-form.tsx
+++ src/app/(admin)/admin/lists/create/list-create-form.tsx
@@ -1,11 +1,8 @@
 "use client";
 
 import BookSelectDialog from "@/src/app/(admin)/admin/lists/create/book-select-dialog";
-import { useBookById } from "@/src/feature/books/queries";
+import DraggableBookList from "@/src/feature/books/components/draggable-book-list";
 import createList from "@/src/feature/lists/api/create-list";
-import NextImage from "@/src/feature/shared/components/next-image";
-import useBoolean from "@/src/feature/shared/hooks/use-boolean";
-import { cn } from "@/src/feature/shared/lib/utils";
 import Button from "@/src/ui/components/button";
 import ErrorMessage from "@/src/ui/components/error-message";
 import { FormRow } from "@/src/ui/components/form";
@@ -13,7 +10,6 @@ import { Input } from "@/src/ui/components/input";
 import Label from "@/src/ui/components/label";
 import { Textarea } from "@/src/ui/components/textarea";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { motion } from "framer-motion";
 import { Suspense } from "react";
 import { useForm } from "react-hook-form";
 import { toast } from "sonner";
@@ -33,7 +29,6 @@ export default function ListCreateForm() {
     watch,
     setValue,
     formState: { errors },
-    reset,
   } = useForm<z.infer<typeof listSchema>>({
     resolver: zodResolver(listSchema),
     defaultValues: {
@@ -63,7 +58,6 @@ export default function ListCreateForm() {
       bookIds.splice(bookIds.indexOf(afterId), 0, bookId);
     }
     setValue("bookIds", bookIds);
-    console.log(`changeBookOrder ${bookId} after ${afterId} to ${bookIds}`);
   };
   const removeBook = (bookId: number) => {
     setValue(
@@ -95,7 +89,7 @@ export default function ListCreateForm() {
       </FormRow>
       <FormRow>
         <h2>책 목록</h2>
-        <BookList
+        <DraggableBookList
           bookIds={watch("bookIds")}
           onRemove={removeBook}
           onMove={changeBookOrder}
@@ -117,174 +111,5 @@ export default function ListCreateForm() {
         <Button type="submit">리스트 생성</Button>
       </FormRow>
     </form>
-  );
-}
-
-interface BookItemProps {
-  bookId: number;
-  onRemove: (bookId: number) => void;
-  onDragStart: (
-    event_: MouseEvent | TouchEvent | PointerEvent,
-    bookId: number,
-  ) => void;
-}
-
-interface BookListProps {
-  bookIds: number[];
-  onRemove: (bookId: number) => void;
-  onMove: (bookId: number, afterId: number) => void;
-}
-
-function BookList({ bookIds, onRemove, onMove }: BookListProps) {
-  const [active, setActive] = useBoolean(false);
-  const highlightIndicator = (event_: React.DragEvent) => {
-    const indicators = getIndicators();
-    clearHighlights(indicators);
-    const element = getNearestIndicator(event_, indicators);
-    element.style.opacity = "1";
-  };
-
-  const getNearestIndicator = (
-    event_: React.DragEvent,
-    indicators: HTMLElement[],
-  ) => {
-    const DISTANCE_THRESHOLD = 45;
-    let closest = {
-      offset: Number.NEGATIVE_INFINITY,
-      element: indicators.at(-1)!,
-    };
-
-    for (const indicator of indicators) {
-      const box = indicator.getBoundingClientRect();
-      const offset = event_.clientY - (box.top + DISTANCE_THRESHOLD);
-
-      if (offset < 0 && offset > closest.offset) {
-        closest = { offset, element: indicator };
-      }
-    }
-
-    return closest.element;
-  };
-
-  const clearHighlights = (elements: HTMLElement[]) => {
-    for (const element of elements) {
-      element.style.opacity = "0";
-    }
-  };
-
-  const getIndicators = () => {
-    return [...document.querySelectorAll<HTMLElement>("[data-after]")];
-  };
-
-  const handleDragStart = (
-    event_: MouseEvent | TouchEvent | PointerEvent,
-    bookId: number,
-  ) => {
-    const event = event_ as unknown as DragEvent;
-    event?.dataTransfer?.setData("bookId", bookId.toString());
-  };
-
-  const handleDragOver = (event_: React.DragEvent) => {
-    event_.preventDefault();
-    highlightIndicator(event_);
-    setActive(true);
-  };
-
-  const handleDragLeave = (event_: React.DragEvent) => {
-    if (event_.currentTarget.contains(event_.relatedTarget as Node)) return;
-    clearHighlights(getIndicators());
-    setActive(false);
-  };
-
-  const handleDragEnd = (event_: React.DragEvent) => {
-    setActive(false);
-    clearHighlights(getIndicators());
-    const bookId = Number(event_.dataTransfer.getData("bookId"));
-    if (!bookId) return;
-
-    const element = getNearestIndicator(event_, getIndicators());
-    const afterId = Number(element.dataset.after || 0);
-    if (afterId === bookId) return;
-
-    onMove(bookId, afterId);
-  };
-
-  return (
-    <ul
-      onDragOver={handleDragOver}
-      onDragLeave={handleDragLeave}
-      onDrop={handleDragEnd}
-      className={cn(active ? "bg-surface-container" : "")}
-    >
-      {bookIds.map((bookId) => (
-        <Suspense key={bookId} fallback={<div>책 정보를 가져오는 중</div>}>
-          <BookItem
-            bookId={bookId}
-            onRemove={onRemove}
-            onDragStart={handleDragStart}
-          />
-        </Suspense>
-      ))}
-      <DropIndicator afterId={0} />
-    </ul>
-  );
-}
-
-function BookItem({ bookId, onRemove, onDragStart }: BookItemProps) {
-  const { data: book, isError } = useBookById(bookId);
-
-  if (!book || isError) {
-    return (
-      <li>
-        <p>책(id:{bookId})을 찾을 수 없습니다.</p>
-        <Button variant="outline" onClick={() => onRemove(bookId)}>
-          삭제
-        </Button>
-      </li>
-    );
-  }
-
-  return (
-    <>
-      <DropIndicator afterId={bookId} />
-      <motion.li
-        className="flex gap-4 items-center justify-between cursor-grab active:cursor-grabbing hover:bg-surface-container rounded-lg p-2"
-        draggable
-        layout
-        data-id={bookId}
-        onDragStart={(event_) => onDragStart(event_, bookId)}
-      >
-        <div className="flex items-center gap-4">
-          <span className="material-icons">drag_indicator</span>
-
-          <NextImage
-            src={book.coverImage}
-            alt={book.title}
-            width={80}
-            height={80}
-            className="w-20 h-20 object-contain rounded-lg"
-          />
-          <p>
-            {book.title}(id:{bookId})
-          </p>
-        </div>
-        <Button
-          type="button"
-          variant="outline"
-          onClick={() => onRemove(bookId)}
-        >
-          삭제
-        </Button>
-      </motion.li>
-    </>
-  );
-}
-
-function DropIndicator({ afterId }: { afterId: number }) {
-  return (
-    <div
-      data-after={afterId || 0}
-      className="my-0.5 h-0.5 w-full bg-primary opacity-0  pointer-events-none"
-    />
   );
 }
