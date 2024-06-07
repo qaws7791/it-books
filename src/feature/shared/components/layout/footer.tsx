fc3a23788a9db70afbe12475cb85fb0bf6d64211
qaws7791 <qaws7791@gmail.com>
2024-06-07 23:52:24 +0900

    Feat: Footer 추가

---
+++ src/feature/shared/components/layout/footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="w-full p-16 bg-surface flex items-center justify-center">
+      <p className=" text-on-surface text-xl">© 2024 it books</p>
+    </footer>
+  );
+}
