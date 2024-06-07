2a414877132fa5237de35396dbbba7a521bf75a4
qaws7791 <qaws7791@gmail.com>
2024-06-07 23:58:39 +0900

    Feat: Pagination을 추가하고, query를 사용

--- src/app/(common)/lists/lists-client.tsx
+++ src/app/(common)/lists/lists-client.tsx
@@ -1,30 +1,43 @@
 "use client";
 import BookCoversPreview from "@/src/feature/books/components/book-covers-preview";
-import { ListPreview } from "@/src/feature/lists/types";
+import { useListsPagination } from "@/src/feature/lists/queries";
+import CommonPagination from "@/src/feature/shared/components/common-pagination";
 import Link from "next/link";
 
 interface ListsClientProps {
-  lists: ListPreview[];
+  page?: number;
+  limit?: number;
 }
 
-export default function ListsClient({ lists }: ListsClientProps) {
-  const books = lists
-    .flatMap((list) => list.listItems)
-    .map((item) => item.book);
+export default function ListsClient({ page, limit }: ListsClientProps) {
+  const {
+    data: { data: lists, pagination },
+  } = useListsPagination({ page, limit });
   return (
-    <div className="grid grid-cols-card-lg gap-4">
-      {lists.map((list) => (
-        <Link
-          href={`/lists/${list.slug}`}
-          key={list.id}
-          className="rounded-xl overflow-hidden hover:bg-outline/10 flex flex-col shadow"
-        >
-          <div className="p-4 rounded-xl ">
-            <BookCoversPreview books={books} count={list.bookCount} />
-          </div>
-          <h2 className="font-medium text-left text-xl p-4">{list.title}</h2>
-        </Link>
-      ))}
+    <div>
+      <div className="grid grid-cols-card-lg gap-4">
+        {lists.map((list) => {
+          const books = list.listItems.flatMap((item) => item.book);
+          return (
+            <Link
+              href={`/lists/${list.slug}`}
+              key={list.id}
+              className="rounded-xl overflow-hidden hover:bg-outline/10 flex flex-col shadow"
+            >
+              <div className="p-4 rounded-xl ">
+                <BookCoversPreview books={books} count={list.bookCount} />
+              </div>
+              <h2 className="font-medium text-left text-xl p-4">
+                {list.title}
+              </h2>
+            </Link>
+          );
+        })}
+      </div>
+      <CommonPagination
+        currentPage={pagination.currentPage}
+        totalPages={pagination.lastPage}
+      />
     </div>
   );
 }
--- src/app/(common)/lists/page.tsx
+++ src/app/(common)/lists/page.tsx
@@ -1,5 +1,4 @@
 import ListsClient from "@/src/app/(common)/lists/lists-client";
-import getListsPagination from "@/src/feature/lists/api/get-lists-pagination";
 import PageContainer from "@/src/feature/shared/components/layout/page-container";
 import { Metadata } from "next";
 
@@ -10,21 +9,15 @@ export const metadata: Metadata = {
 interface ListsPageProps {
   searchParams: {
     page?: string;
-    limit?: string;
   };
 }
 
-export default async function CollectionsPage({
-  searchParams,
-}: ListsPageProps) {
+export default function CollectionsPage({ searchParams }: ListsPageProps) {
   const page = searchParams.page ? Number.parseInt(searchParams.page) : 1;
-  const limit = searchParams.limit ? Number.parseInt(searchParams.limit) : 10;
-  const { data } = await getListsPagination({ page, limit });
-
   return (
     <PageContainer>
       <h1 className="sr-only">Collections</h1>
-      <ListsClient lists={data} />
+      <ListsClient page={page} limit={12} />
     </PageContainer>
   );
 }
