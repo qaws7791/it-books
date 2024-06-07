fc2d77dc55c76ae56e88a5a77e46d9405fe1b154
qaws7791 <qaws7791@gmail.com>
2024-06-07 23:49:21 +0900

    Feat: BookList 추가

---
+++ src/app/(common)/books/book-list.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import BookPreview from "@/src/feature/books/components/book-preview";
+import { useBooksPagination } from "@/src/feature/books/queries";
+import CommonPagination from "@/src/feature/shared/components/common-pagination";
+
+interface BooksListProps {
+  page?: number;
+  limit?: number;
+}
+
+export default function BookList({ page, limit }: BooksListProps) {
+  const {
+    data: { data: books, pagination },
+  } = useBooksPagination({ page, limit });
+
+  return (
+    <div>
+      <div className="grid grid-cols-card gap-4">
+        {books.map((book) => (
+          <BookPreview key={book.slug} book={book} />
+        ))}
+      </div>
+      <CommonPagination
+        currentPage={pagination.currentPage}
+        totalPages={pagination.lastPage}
+      />
+    </div>
+  );
+}
