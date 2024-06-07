e68656e10e87dfb2048993fd5c0848f41b45ab70
qaws7791 <qaws7791@gmail.com>
2024-06-07 23:51:39 +0900

    Feat: SITE_URL 변경

--- src/feature/shared/constants/site-config.constant.ts
+++ src/feature/shared/constants/site-config.constant.ts
@@ -1,4 +1,4 @@
 export const SITE_NAME = "IT Books";
 export const SITE_DESCRIPTION = "한국어 IT 도서 정보 사이트";
-export const SITE_URL = "https://my-site.com";
+export const SITE_URL = "http://localhost:3000";
 export const SITE_KEYWORDS = ["site", "my-site", "my-site.com"];
