4c5049835c8a0cdbb9d520afc6cb46bef284a3aa
qaws7791 <qaws7791@gmail.com>
2024-06-08 23:59:47 +0900

    Feat: Footer 링크들 추가

--- src/feature/shared/components/layout/footer.tsx
+++ src/feature/shared/components/layout/footer.tsx
@@ -1,7 +1,28 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Footer() {
   return (
-    <footer className="w-full p-16 bg-surface flex items-center justify-center">
-      <p className=" text-on-surface text-xl">© 2024 it books</p>
+    <footer className="w-full px-8 py-8 pt-24 flex items-center justify-between gap-4">
+      <div className="flex items-center gap-8">
+        <Image src="/logo.svg" alt="logo" width={160} height={40} />
+        <div className="flex gap-4">
+          <Link href="/books" className="hover:underline">
+            책 찾기
+          </Link>
+          <Link href="/lists" className="hover:underline">
+            리스트 찾기
+          </Link>
+        </div>
+      </div>
+      <div className="flex gap-4">
+        <Link href="/terms" className="hover:underline">
+          서비스 이용약관
+        </Link>
+        <Link href="/privacy" className="hover:underline">
+          개인정보 처리방침
+        </Link>
+      </div>
     </footer>
   );
 }
