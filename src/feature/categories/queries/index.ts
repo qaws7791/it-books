635ab68b3c07fb94a6b036e4db14e1f2eb31c23a
qaws7791 <qaws7791@gmail.com>
2024-06-09 22:56:20 +0900

    Feat:  category pagination에서 검색 지원

--- src/feature/categories/queries/index.ts
+++ src/feature/categories/queries/index.ts
@@ -1,14 +1,27 @@
 import { getCategories } from "@/src/feature/categories/api/get-categories";
+import { CategorySearchParams } from "@/src/feature/categories/types";
 import { createQueryKeys } from "@lukemorales/query-key-factory";
 import { useSuspenseQuery } from "@tanstack/react-query";
 
 export const categoryQueryKeys = createQueryKeys("categories", {
   fetchCategories: () => ["categories"],
+  fetchCategoriesPagination: (params: CategorySearchParams) => [
+    "search",
+    params,
+  ],
 });
 
-export const useCategoriesQuery = () => {
+export const useCategoriesQuery = ({
+  page = 1,
+  limit = 10,
+  query,
+}: CategorySearchParams) => {
   return useSuspenseQuery({
-    queryKey: categoryQueryKeys.fetchCategories().queryKey,
-    queryFn: getCategories,
+    queryKey: categoryQueryKeys.fetchCategoriesPagination({
+      page,
+      limit,
+      query,
+    }).queryKey,
+    queryFn: () => getCategories({ page, limit, query }),
   });
 };
