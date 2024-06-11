8be3dac0be437e467d1e15fe1e16ba229d9a1c41
qaws7791 <qaws7791@gmail.com>
2024-06-11 23:52:49 +0900

    Feat: useCreateBookMutation추가

--- src/feature/books/api/create-book.ts
+++ src/feature/books/api/create-book.ts
@@ -1,5 +1,8 @@
 import { BookStatus } from "@/src/feature/books/constants";
+import { bookQueryKeys } from "@/src/feature/books/queries";
 import api from "@/src/feature/shared/api";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { toast } from "sonner";
 
 export interface CreateBookInput {
   title: string;
@@ -24,8 +27,8 @@ interface CreateBookOutput {
   isbn: string;
   description: string;
   coverImage: string;
-  authors: string;
-  translator: string;
+  authors: string[];
+  translator: string[];
   publisher: string;
   publishedDate: string;
   status: BookStatus;
@@ -37,3 +40,19 @@ export default function createBook(
 ): Promise<CreateBookOutput> {
   return api.post("/books", input);
 }
+
+export const useCreateBookMutation = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: createBook,
+    onSuccess: () => {
+      void queryClient.invalidateQueries({
+        queryKey: bookQueryKeys._def,
+      });
+      toast.success("책을 성공적으로 추가했습니다.");
+    },
+    onError: () => {
+      toast.error("책 추가에 실패했습니다.");
+    },
+  });
+};
