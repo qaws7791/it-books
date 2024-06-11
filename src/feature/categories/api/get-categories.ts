686997ec1fdaeaf6706cccfb38501c3936836779
qaws7791 <qaws7791@gmail.com>
2024-06-11 23:47:52 +0900

    Feat:getCategories에 pagination 및 query 추가

--- src/feature/categories/api/get-categories.ts
+++ src/feature/categories/api/get-categories.ts
@@ -1,10 +1,18 @@
-import { Category } from "@/src/feature/categories/types";
+import { Category, CategorySearchParams } from "@/src/feature/categories/types";
 import api from "@/src/feature/shared/api";
+import { PaginationResponse } from "@/src/feature/shared/type/api";
 
 interface GetCategoriesResponse {
   data: Category[];
+  pagination: PaginationResponse;
 }
 
-export const getCategories = (): Promise<GetCategoriesResponse> => {
-  return api.get("/categories");
+export const getCategories = ({
+  page,
+  limit,
+  query,
+}: CategorySearchParams): Promise<GetCategoriesResponse> => {
+  return api.get("/categories", {
+    params: { page, limit, query },
+  });
 };
