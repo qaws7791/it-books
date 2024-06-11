b0ce8ff144edbc919f71ff916f85f0ccfbfc951c
qaws7791 <qaws7791@gmail.com>
2024-06-11 23:41:41 +0900

    Feat: useCategoryQuery 추가

--- src/feature/categories/queries/index.ts
+++ src/feature/categories/queries/index.ts
@@ -1,14 +1,15 @@
 import { getCategories } from "@/src/feature/categories/api/get-categories";
+import { getCategory } from "@/src/feature/categories/api/get-category";
 import { CategorySearchParams } from "@/src/feature/categories/types";
 import { createQueryKeys } from "@lukemorales/query-key-factory";
 import { useSuspenseQuery } from "@tanstack/react-query";
 
 export const categoryQueryKeys = createQueryKeys("categories", {
-  fetchCategories: () => ["categories"],
   fetchCategoriesPagination: (params: CategorySearchParams) => [
     "search",
     params,
   ],
+  fetchCategory: (categoryId: number) => [categoryId],
 });
 
 export const useCategoriesQuery = ({
@@ -23,5 +24,12 @@ export const useCategoriesQuery = ({
       query,
     }).queryKey,
     queryFn: () => getCategories({ page, limit, query }),
+  });
+};
+
+export const useCategoryQuery = (categoryId: number) => {
+  return useSuspenseQuery({
+    queryKey: categoryQueryKeys.fetchCategory(categoryId).queryKey,
+    queryFn: () => getCategory(categoryId),
   });
 };
