bc66d77d9a87650cb84ef8c5cb470e3898b52f20
qaws7791 <qaws7791@gmail.com>
2024-06-13 04:28:05 +0900

    Feat: 카테고리 추가

--- src/app/(common)/books/page.tsx
+++ src/app/(common)/books/page.tsx
@@ -1,24 +1,45 @@
 import BookList from "@/src/app/(common)/books/book-list";
+import LocalCategoryList from "@/src/feature/categories/components/local-category-list";
+import { getLocalCategory } from "@/src/feature/categories/helpers";
 import PageContainer from "@/src/feature/shared/components/layout/page-container";
 import { Metadata } from "next";
+
+// categories about it books
 
 interface BooksPageProps {
   searchParams: {
     page?: string;
+    category?: string;
   };
 }
 
-export const metadata: Metadata = {
-  title: "책 목록",
-};
+export function generateMetadata({ searchParams }: BooksPageProps): Metadata {
+  const category = searchParams.category
+    ? getLocalCategory(searchParams.category)
+    : null;
+  const title = category ? `${category.name}` : "책 목록";
+  return {
+    title: title,
+  };
+}
 
 export default function BooksPage({ searchParams }: BooksPageProps) {
   const page = searchParams.page ? Number.parseInt(searchParams.page) : 1;
+  const categorySlug = searchParams["category"];
+  const category = categorySlug ? getLocalCategory(categorySlug) : undefined;
 
   return (
     <PageContainer>
       <h1 className="sr-only">Books</h1>
-      <BookList page={page} limit={12} />
+      <section className="flex items-center justify-center w-full h-96 rounded-3xl bg-surface-container mb-16 ">
+        <h2 className="text-4xl font-bold">
+          {category ? category.name : "책 목록"}
+        </h2>
+      </section>
+      <h2 className="text-2xl font-bold my-8">카테고리</h2>
+      <LocalCategoryList currentCategory={category} />
+      <h2 className="text-2xl font-bold my-8">책 목록</h2>
+      <BookList page={page} limit={12} categorySlug={categorySlug} />
     </PageContainer>
   );
 }
