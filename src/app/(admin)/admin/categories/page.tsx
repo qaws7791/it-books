d2d41ac3fdf0a25f082090c757f02274e0b89366
qaws7791 <qaws7791@gmail.com>
2024-06-13 04:23:26 +0900

    Feat: 카테고리 테이블을 사용하는 페이지로 변경

--- src/app/(admin)/admin/categories/page.tsx
+++ src/app/(admin)/admin/categories/page.tsx
@@ -1,13 +1,26 @@
-import CategoryForm from "@/src/app/(admin)/admin/categories/_components/category-form";
-import CategoryList from "@/src/app/(admin)/admin/categories/_components/category-list";
+import CategoryTable from "@/src/feature/categories/components/category-table";
 import PageContainer from "@/src/feature/shared/components/layout/page-container";
+import Button from "@/src/ui/components/button";
+import { Metadata } from "next";
+import Link from "next/link";
+import { Suspense } from "react";
+
+export const metadata: Metadata = {
+  title: "카테고리 관리",
+};
 
 export default function AdminCategoriesPage() {
   return (
     <PageContainer>
-      <h1 className="text-2xl font-bold">카테고리 관리</h1>
-      <CategoryList />
-      <CategoryForm />
+      <header className="flex justify-between items-center mb-4">
+        <h1 className="text-2xl font-medium">카테고리 관리</h1>
+        <Button asChild>
+          <Link href="/admin/categories/create">카테고리 추가하기</Link>
+        </Button>
+      </header>
+      <Suspense fallback="loading...">
+        <CategoryTable />
+      </Suspense>
     </PageContainer>
   );
 }
