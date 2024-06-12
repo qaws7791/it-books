efaddf5d222a41f7674b397899b3e66ec82817a5
qaws7791 <qaws7791@gmail.com>
2024-06-12 23:56:30 +0900

    Design: 최소 높이 지정

--- src/feature/shared/components/layout/page-container.tsx
+++ src/feature/shared/components/layout/page-container.tsx
@@ -8,7 +8,10 @@ export default function PageContainer({
 }: React.HTMLAttributes<HTMLDivElement>) {
   return (
     <div
-      className={cn("w-full max-w-screen-xl mx-auto p-2 md:p-4", className)}
+      className={cn(
+        "w-full max-w-screen-xl mx-auto p-2 md:p-4 min-h-screen",
+        className,
+      )}
       {...properties}
     >
       <div>{children}</div>
