b9de573b228561650df18ee39ec9289395efadf0
qaws7791 <qaws7791@gmail.com>
2024-06-23 19:01:00 +0900

    Feat: getUserLikes api 추가

--- src/feature/user/queries/index.ts
+++ src/feature/user/queries/index.ts
@@ -1,9 +1,38 @@
-import { getProfile } from "@/src/feature/user/api";
-import { useQuery } from "@tanstack/react-query";
+import {
+  getProfile,
+  getUserLikesPagination,
+  getUserLikesPaginationInput,
+} from "@/src/feature/user/api";
+import { createQueryKeys } from "@lukemorales/query-key-factory";
+import { useQuery, useSuspenseInfiniteQuery } from "@tanstack/react-query";
+
+export const userQueryKeys = createQueryKeys("user", {
+  fetchProfile: () => ["profile"],
+  fetchLikes: ({ userId, limit, page }: getUserLikesPaginationInput) => [
+    "likes",
+    { userId, limit, page },
+  ],
+});
 
 export const useUserProfile = () => {
   return useQuery({
-    queryKey: ["user", "profile"],
+    queryKey: userQueryKeys.fetchProfile().queryKey,
     queryFn: getProfile,
+  });
+};
+
+export const useUserLikesPagination = ({
+  page = 1,
+  limit = 10,
+  userId,
+}: getUserLikesPaginationInput) => {
+  return useSuspenseInfiniteQuery({
+    queryKey: userQueryKeys.fetchLikes({ page, limit, userId }).queryKey,
+    queryFn: ({ pageParam }) =>
+      getUserLikesPagination({ page: pageParam, limit, userId }),
+    initialPageParam: page,
+    getNextPageParam: (lastPage) => {
+      return lastPage.pagination.nextPage;
+    },
   });
 };
