d28b36bae3cc05a7b9a019b5bc57cffc52a0ae62
qaws7791 <qaws7791@gmail.com>
2024-06-24 21:34:20 +0900

    Design: Footer 모바일을 위한 디자인 수정

--- src/feature/shared/components/layout/footer.tsx
+++ src/feature/shared/components/layout/footer.tsx
@@ -3,9 +3,10 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="w-full px-8 py-8 pt-24 flex items-center justify-between gap-4">
-      <div className="flex items-center gap-8">
+    <footer className="w-full px-8 py-8 pt-16 flex flex-col items-center justify-start gap-4 md:justify-between border-t-4 border-double border-outline/40 md:flex-row ">
+      <div className="flex flex-col items-center gap-4 md:flex-row">
         <Image src="/logo.svg" alt="logo" width={160} height={40} />
+
         <div className="flex gap-4">
           <Link href="/books" className="hover:underline">
             책 찾기
@@ -15,7 +16,7 @@ export default function Footer() {
           </Link>
         </div>
       </div>
-      <div className="flex gap-4">
+      <div className="flex items-center gap-4">
         <Link href="/terms" className="hover:underline">
           서비스 이용약관
         </Link>
