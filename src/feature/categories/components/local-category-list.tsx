55aa2c6c30db6a14b3afe280e731f74d990d70b7
qaws7791 <qaws7791@gmail.com>
2024-07-02 23:34:37 +0900

    Feat: 카테고리를 보여주기만 하는 컴포넌트로 변경

--- src/feature/categories/components/local-category-list.tsx
+++ src/feature/categories/components/local-category-list.tsx
@@ -3,45 +3,27 @@ import { LocalCategory } from "@/src/feature/categories/types";
 import { cn } from "@/src/feature/shared/lib/utils";
 import Link from "next/link";
 
-interface CategoriesProps {
-  currentCategory?: LocalCategory;
-}
+interface CategoriesProps {}
 
-export default function LocalCategoryList({
-  currentCategory,
-}: CategoriesProps) {
+export default function LocalCategoryList({}: CategoriesProps) {
   return (
     <div className="grid grid-cols-2 sm:grid-cols-card gap-4 p-2 justify-items-center">
-      <CategoryItem
-        key="all"
-        name="전체"
-        slug=""
-        iconName="all_inclusive"
-        selected={!currentCategory}
-      />
       {LOCAL_CATEGORIES.map((category) => (
-        <CategoryItem
-          key={category.slug}
-          {...category}
-          selected={category === currentCategory}
-        />
+        <CategoryItem key={category.slug} {...category} />
       ))}
     </div>
   );
 }
 
-interface CategoryItemProps extends LocalCategory {
-  selected?: boolean;
-}
+interface CategoryItemProps extends LocalCategory {}
 
-function CategoryItem({ selected, slug, name, iconName }: CategoryItemProps) {
+function CategoryItem({ slug, name, iconName }: CategoryItemProps) {
   return (
     <Link
       key={slug}
       href={"/books" + (slug ? `?category=${slug}` : "")}
       className={cn(
-        "flex items-center justify-between gap-2 rounded-2xl border border-outline text-on-tertiary-container p-4 w-full sm:w-64 box-border hover:shadow-1 transition-shadow",
-        selected ? "bg-primary-container/40" : "bg-surface-container",
+        "flex items-center justify-between gap-2 rounded-2xl border border-outline text-on-tertiary-container p-4 w-full sm:w-64 box-border hover:shadow-1 transition-shadow bg-surface-container",
       )}
     >
       <span>{name}</span>
