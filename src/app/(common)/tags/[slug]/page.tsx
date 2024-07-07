a3532c699522d7e2a4bdc9d2b1ed9ddbe4ebee67
qaws7791 <qaws7791@gmail.com>
2024-07-07 20:09:33 +0900

    Feat: tags 페이지 api 연결

--- src/app/(common)/tags/[slug]/page.tsx
+++ src/app/(common)/tags/[slug]/page.tsx
@@ -1,12 +1,13 @@
-import DUMMY from "@/src/dummy";
+import BookList from "@/src/app/(common)/books/book-list";
+import fetchBooks from "@/src/feature/books/api/fetch-books";
+import CommonPagination from "@/src/feature/shared/components/common-pagination";
 import PageContainer from "@/src/feature/shared/components/layout/page-container";
-import NextImage from "@/src/feature/shared/components/next-image";
 import { Metadata } from "next";
-import Link from "next/link";
 
 interface BookDetailPageProperties {
   params: {
     slug: string;
+    page?: string;
   };
 }
 
@@ -20,42 +21,22 @@ export function generateMetadata({
   };
 }
 
-const getBookByTag = (tag: string) => {
-  return DUMMY.BOOKS.filter((book) =>
-    book.tags.some((t) => t.name.includes(tag)),
-  );
-};
-
-export default function TaggedBooksPage({ params }: BookDetailPageProperties) {
+export default async function TaggedBooksPage({
+  params,
+}: BookDetailPageProperties) {
   const decodedSlug = decodeURIComponent(params.slug);
-  const books = getBookByTag(decodedSlug);
+  const page = params.page ? Number.parseInt(params.page, 10) : 1;
+  const { data: books, pagination } = await fetchBooks({
+    tag: decodedSlug,
+    page: 1,
+    limit: 20,
+  });
 
   return (
     <PageContainer>
-      <h1 className="text-4xl text-center">
-        &quot;{decodedSlug}&quot; 태그를 가진 책들
-      </h1>
-      <div className="grid grid-cols-card gap-4 overflow-y-auto mt-4">
-        {books.map((book) => (
-          <Link
-            href={`/books/${book.slug}`}
-            key={book.id}
-            className="rounded-xl overflow-hidden hover:bg-outline/10 p-4"
-          >
-            <div>
-              <NextImage
-                src={book.coverImage}
-                alt={book.title}
-                className="shadow-md mx-auto"
-                width={200}
-                height={300}
-              />
-            </div>
-            <h2 className="font-bold text-center mt-2">{book.title}</h2>
-            <p className="text-outline text-center">{book.authors}</p>
-          </Link>
-        ))}
-      </div>
+      <h1 className="text-4xl text-center my-8">#{decodedSlug}</h1>
+      <BookList books={books} />
+      <CommonPagination currentPage={page} totalPages={pagination.lastPage} />
     </PageContainer>
   );
 }
