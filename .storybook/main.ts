f5861ccfb4a18166dcb2612e1f712cc580a93787
qaws7791 <qaws7791@gmail.com>
2024-09-15 01:07:33 +0900

    Refactor: Update Storybook configuration

---
+++ .storybook/main.ts
@@ -0,0 +1,57 @@
+import type { StorybookConfig } from "@storybook/nextjs";
+
+const config: StorybookConfig = {
+  stories: [
+    "../stories/**/*.mdx",
+    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+  ],
+
+  addons: [
+    "@storybook/addon-onboarding",
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@chromatic-com/storybook",
+    "@storybook/addon-interactions",
+    "@storybook/addon-controls",
+    {
+      name: "@storybook/addon-styling-webpack",
+      options: {
+        rules: [
+          {
+            test: /\.css$/,
+            sideEffects: true,
+            use: [
+              require.resolve("style-loader"),
+              {
+                loader: require.resolve("css-loader"),
+                options: {
+                  importLoaders: 1,
+                },
+              },
+              {
+                loader: require.resolve("postcss-loader"),
+                options: {
+                  implementation: require.resolve("postcss"),
+                },
+              },
+            ],
+          },
+        ],
+      },
+    },
+  ],
+
+  framework: {
+    name: "@storybook/nextjs",
+    options: {},
+  },
+
+  docs: {},
+
+  staticDirs: ["..\\public"],
+
+  typescript: {
+    reactDocgen: "react-docgen-typescript",
+  },
+};
+export default config;
