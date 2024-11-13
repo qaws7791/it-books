4f175c3025b002017141999de4eca765e0c2905a
qaws7791 <qaws7791@gmail.com>
2024-11-13 19:29:27 +0900

    Style: 페이지 레이아웃 조정

--- src/app/(admin)/admin/books/create/page.tsx
+++ src/app/(admin)/admin/books/create/page.tsx
@@ -4,8 +4,10 @@ import PageContainer from "@/src/feature/shared/components/layout/page-container
 export default function AdminBookCreatePage() {
   return (
     <PageContainer>
-      <h1 className="text-2xl font-bold">도서 추가</h1>
-      <BookCreateForm />
+      <div className="max-w-screen-md mx-auto">
+        <h1 className="text-2xl font-bold my-4">도서 추가</h1>
+        <BookCreateForm />
+      </div>
     </PageContainer>
   );
 }
